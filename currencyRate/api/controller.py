9d1a40b161341dda642dc04244d7b475eef758c2
EmilzonJ <emilzonujero@gmail.com>
2022-04-15 03:52:38 -0600

    Refactor

--- currencyRate/api/controller.py
+++ currencyRate/api/controller.py
@@ -5,42 +5,36 @@ from currencyRate.services.scraper import get_occidente_currency_rates, get_fico
 from currencyRate.api.responses.models import BankResponse
 
 router = APIRouter(
-    prefix="/currency"
+    tags=["Currency Rates"],
+    prefix="/currency_rates"
 )
 
 
 @router.get("/occ", response_model=BankResponse)
 async def get_occidente_currency_rate():
-    occidente = await get_occidente_currency_rates()
-    return occidente
+    return await get_occidente_currency_rates()
 
 
 @router.get("/fic", response_model=BankResponse)
 async def get_ficohsa_currency_rate():
-    occidente = await get_ficohsa_currency_rates()
-    return occidente
+    return await get_ficohsa_currency_rates()
 
 
 @router.get("/bnp", response_model=BankResponse)
 async def get_banpais_currency_rate():
-    occidente = await get_banpais_currency_rates()
-    return occidente
+    return await get_banpais_currency_rates()
 
 
 @router.get("/atl", response_model=BankResponse)
 async def get_atlantida_currency_rate():
-    occidente = await get_atlantida_currency_rates()
-    return occidente
+    return await get_atlantida_currency_rates()
 
 
-@router.get("/get_rates", response_model=list[BankResponse])
+@router.get("/get_all", response_model=list[BankResponse])
 async def get_all_banks_currency_rates():
     occidente = await get_occidente_currency_rates()
     ficohsa = await get_ficohsa_currency_rates()
     banpais = await get_banpais_currency_rates()
     atlantida = await get_atlantida_currency_rates()
 
-    return [occidente,
-            ficohsa,
-            banpais,
-            atlantida]
+    return [occidente, ficohsa, banpais, atlantida]
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi import APIRouter
 from currencyRate.api import controller as currency_rate
 
-app = FastAPI()
+app = FastAPI(title="Currency Rates HN API", version="1.0.0")
 
 router = APIRouter(
     prefix="/api/v1"
